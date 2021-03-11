fc9d8fd1defff266e0d120de1280de116773fe89
Slim Yelow <slimyelow@gmail.com>
2021-03-11 13:18:00 +0100

    add counter badge to favorites tab in nav bar

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,7 +1,10 @@
 import { Link } from 'react-router-dom';
 import classes from './MainNavigation.module.css';
+import { useContext } from 'react';
+import FavoritesContext, { FavoritesContextProvider } from '../../store/favorites-context';
 
 const MainNavigation = () => {
+  const favoritesCtx = useContext(FavoritesContext);
   return (
     <header className={classes.header}>
       <h1 className={classes.logo}>React Meetups</h1>
@@ -14,7 +17,10 @@ const MainNavigation = () => {
             <Link to="/new-meetup">Add New Meetup</Link>
           </li>
           <li>
-            <Link to="/favorites">My Favorites</Link>
+            <Link to="/favorites">
+              My Favorites
+              <span className={classes.badge}>{favoritesCtx.totalFavorites}</span>
+            </Link>
           </li>
         </ul>
       </nav>
